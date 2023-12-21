fea402a9a8ee6c30cfb897f49823bd5d37b6d658
Ales Pour <pourales@gmail.com>
2023-12-21 14:20:19 +0100

    Had simple

--- HadLED/HadLED.c
+++ HadLED/HadLED.c
@@ -5,37 +5,11 @@
  *  Author: Ales
  */ 
 
+#define F_CPU 12000000
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
-void doPause()
-{
-	int i=100;
-	int y=1000;
-	while (i>0) 
-	{
-		i--;
-		y=1000;
-		while (y>0)
-		{
-			y--;
-		}
-	}
-}
-
-void doPauseQuick()
-{
-	int i=1;
-	int y=1000;
-	while (i>0) 
-	{
-		i--;
-		y=1;
-		while (y>0)
-		{
-			y--;
-		}
-	}
-}
+#include <util/delay.h>
 
 void had()
 {
@@ -50,7 +24,7 @@ void had()
 	int mask = 0x01;
     while(1)
     {
-		doPause();
+		_delay_ms(500);
 		mask<<=1;
 		if (mask==0x100) 
 		{
@@ -63,50 +37,7 @@ void had()
     }
 }
 
-void testPin()
-{
-	PORTB = 0x20;   /* activate all pull-ups */
-    DDRB = 0xff;    /* all pins output */ 
-    while(1)
-	{
-		;
-	}	
-}
-
-void ledFlashing()
-{
-	PORTB = 0x20;   /* activate all pull-ups */
-    DDRB = 0xff;    /* all pins output */ 	
-	int val = 0x01;
-    while(1)
-    {
-		doPauseQuick();
-		PORTB=~(val&0xFF);
-		val = (val==0x01) ? 0x0 : 0x01;
-    }
-}
-
-
-#include <avr/interrupt.h>
-short timer0Cnt;
-
-void timer0Init()
-   {
-   // clear timer0Cntr
-   timer0Cnt=0;
-   // enable interrupt from timer 0 
-   TIMSK = TIMSK | _BV(TOIE0);
-   // set CLOCK / 8
-   TCCR0 = _BV(CS01);
-   // set counter
-   TCNT0 = 106;
-   sei();
-   }
-
 int main(void)
 {
-	timer0Init();
 	had();
-	//testPin();
-	ledFlashing();
 }
