d43f6908a0651cf50794a8cb7eb2e893855bcb1a
Ales Pour <apour@seznam.cz>
2025-01-31 22:09:05 +0100

    Adjusted on model

--- SpinacRedukceRoman/SpinacRedukceRoman/SpinacRedukceRoman.c
+++ SpinacRedukceRoman/SpinacRedukceRoman/SpinacRedukceRoman.c
@@ -1,14 +1,19 @@
+#define F_CPU 16000000UL  // Define the clock frequency (8 MHz)
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#define F_CPU 16000000UL  // Define the clock frequency (8 MHz)
-
 // LED connected to PB0
-#define LED_PIN PB0
+#define LED_PIN			PB0
+#define RELAY_PIN		PB1
+#define TCNT0_FOR_1S	256-244
 
 volatile unsigned int repeat_cnt0 = 0;
 volatile unsigned int frequency = 0;
+volatile unsigned int frequencyTemp = 0;
+typedef enum { t_relayOn, t_relayOff } TRelayMode;
+TRelayMode relayMode;
 
 void interrupt0_init()
 {
@@ -21,39 +26,43 @@ void interrupt0_init()
 
 ISR (INT0_vect)
 {	
-	int n=5;
-	n++;	
 	frequency++;
-	//if (PORTB==1)
-				//PORTB = 0;
-			//else
-	//PORTB = 1;
+}
+
+void switchOnRelay()
+{
+	if (relayMode == t_relayOn) return;
+	PORTB|= (1<<RELAY_PIN);
+	relayMode = t_relayOn;
+}
+
+void switchOffRelay()
+{
+	if (relayMode == t_relayOff) return;
+	PORTB&=~(1<<RELAY_PIN);
+	relayMode = t_relayOff;
 }
 
 // Function to initialize Timer0 to generate interrupts
 void timer0_init()
 {
-	TCCR0A = 0x05;
-	TCCR0B = 0x04;
-	TCNT0 = 256 - 244;
+	TCCR0B = 0x04;			// clock / 256
+	TCNT0 = TCNT0_FOR_1S;
 	TIMSK |= _BV(TOIE0);   // enable interrupt for compare A
 }
 
 ISR (TIMER0_OVF_vect)
 {
-	TCNT0 = 256 - 244;
-    if (++repeat_cnt0 == 64)
+	TCNT0 = TCNT0_FOR_1S;
+    if (++repeat_cnt0 == 256)
 	{
 		repeat_cnt0 = 0;
-		//PORTB = 0;
-		if (frequency > 5)
-		{
-			if (PORTB==1)
-				PORTB = 0;
-			else
-				PORTB = 1;		
-		}	
+		frequencyTemp = frequency;
 		frequency = 0;
+		if (frequencyTemp > 40)
+			switchOffRelay();
+		else
+			switchOnRelay();
 	}	
 }
 
@@ -61,8 +70,10 @@ int main()
 {
     // Set PB0 (LED) as an output pin
 	DDRB = 0;
-    DDRB |= (1 << LED_PIN);
+    DDRB |= (1 << LED_PIN) | (1 << RELAY_PIN);
     PORTB = 0;
+	relayMode = t_relayOff;
+	switchOnRelay();
 	
     // Initialize Timer0
     timer0_init();
@@ -72,6 +83,5 @@ int main()
     // Main loop
     while (1)
     {					
-        // Main loop does nothing, Timer0 interrupt handles the LED flashing
     }
 }
