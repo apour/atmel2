db09f7d4bee4828359b611236bedece4755f1722
Ales Pour <a.pour@quadient.com>
2025-03-06 19:53:17 +0100

    Code cleanup

--- WireFinder/WireFinder/WireFinderQuick.c
+++ WireFinder/WireFinder/WireFinderQuick.c
@@ -9,42 +9,11 @@
 #include <avr/interrupt.h>
 
 #define TIMER0_CONST	256-20
-#define TIMER1_CONST	65536-6000//65536-16000
-#define WIRES_COUNT	16
+#define TIMER1_CONST	65536-6000
 
-#define BUFFER_SIZE	6
-char displayBuffer[BUFFER_SIZE];
-unsigned int dividers[] = {10000, 1000, 100, 10, 1, 0};
-
-volatile unsigned short wire[WIRES_COUNT];
 unsigned short curWireValuePortD;
 unsigned short curWireValuePortC;
 unsigned short mask;
-unsigned short lock=0;
-
-void convertNumber(unsigned int number)
-{
-	number++;
-	unsigned int* divider = dividers;
-	unsigned short v = 0;
-	char* p = displayBuffer;
-	unsigned int curDivider = 0;
-	while (*divider > 0)
-	{
-		curDivider = *divider;
-		v = 0;
-		while (number>curDivider)
-		{
-			number-= curDivider;
-			v++;
-		}
-		v+= 0x30;
-		*p = v;
-		divider++;
-		p++;
-	}	
-	*p = '\0';
-}
 
 void init()
 {
@@ -66,7 +35,6 @@ void timer0Init()
 
 ISR (TIMER0_OVF_vect)
 {
-	//lock = 1;
 	TCNT0 = TIMER0_CONST;
 	curWireValuePortD++;
 	if (curWireValuePortD==0xFF)
@@ -74,14 +42,13 @@ ISR (TIMER0_OVF_vect)
 		curWireValuePortD = 0;
 	}
 	PORTD = curWireValuePortD;	
-	//lock = 0;
 }
 
 void timer1Init()
 {
 	// Set Timer1 to normal mode
 	TCCR1A = 0x00;  // Normal mode, no PWM or CTC
-	TCCR1B = 1;  // 64 // Prescaler of 8 (CS11 = 1, CS10 = 0, CS12 = 0)
+	TCCR1B = 0x01;  // Prescaler of 8 (CS11 = 1, CS10 = 0, CS12 = 0)
 
 	// Optional: Enable overflow interrupt (if you want to use interrupts)
 	TIMSK |= (1 << TOIE1);  // Enable Timer1 overflow interrupt
