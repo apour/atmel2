68abd980a97ad7a8172f1663f5f6cc5adef00aa5
Ales Pour <a.pour@quadient.com>
2026-03-13 19:21:11 +0100

    Frequency Generator works with ADC

--- FrGeneratorAIGemini/FrequencyGeneratorAIGemini.c
+++ FrGeneratorAIGemini/FrequencyGeneratorAIGemini.c
@@ -3,10 +3,9 @@
 #include <avr/io.h>
 #include <avr/delay.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
 
-
-volatile uint16_t last_capture = 0;
-volatile uint16_t period_ticks = 0;
+//#define DUMP_TO_UART    1
 
 volatile uint8_t timer_overflows = 0;
 volatile uint32_t final_time = 0;
@@ -16,6 +15,7 @@ volatile uint8_t overlowMeasure = 0;
 
 volatile uint8_t lastTimeHi = 0;
 volatile uint8_t lastTimeLo = 0;
+volatile uint16_t last_adc_value = 0;
 
 // Přerušení při přetečení 8-bitového časovače
 ISR(TIMER0_OVF_vect) {
@@ -25,8 +25,8 @@ ISR(TIMER0_OVF_vect) {
     if (timer_overflows == 0xFF) {
         overlowMeasure = 1;
         timer_overflows = 0;
-    }
-    //uart_send_char('O');
+        frequencyOutputDisable(); // zastavení výstupu frekvence při přetečení
+    }    
 }
 
 // Přerušení na INT0 (PD2) - měření vstupní frekvence
@@ -38,6 +38,9 @@ ISR(INT0_vect) {
         TCNT0 = 0;
         timer_overflows = 0;
         edge_count = 1;
+        if (overlowMeasure == 1 ) {
+            frequencyOutputEnable(); // znovu povolit výstup frekvence po přetečení
+        }
         overlowMeasure = 0;
         //uart_send_char('S');        
     } else {
@@ -48,6 +51,20 @@ ISR(INT0_vect) {
         final_time/= 2; // protože měříme periodu, ne frekvenci, vynásobíme 2    
         edge_count = 0; 
     
+        // calculate by ADC - Begin
+        uint32_t temp_final_time = final_time>>2;
+        if (last_adc_value < 512) 
+        {
+            // slow down
+            temp_final_time = (temp_final_time * (512-last_adc_value))>>9; // úprava podle ADC hodnoty
+            final_time+= temp_final_time; // přidáme zpomalení k původní hodnotě
+        } else {            
+            // speed up
+            temp_final_time = (temp_final_time * (last_adc_value-512))>>9; // úprava podle ADC hodnoty
+            final_time-= temp_final_time; // přidáme zrychlení k původní hodnotě
+        }
+        // calculate by ADC - End
+
 		OCR1AH = final_time >> 8; // vyšší byte
 		OCR1AL = final_time & 0xFF; // nižší byte
 		        
@@ -59,10 +76,35 @@ ISR(INT0_vect) {
     inMeasureCalculate = 0;
 }
 
-void main(void) {
-    // 1. Nastavení pinu PB1 (OC1A) jako výstupní
-    DDRB |= (1 << PB1);
+ISR(ADC_vect) {
+    // Read ADC value
+    uint16_t adc_value = ADC; // ADC value is now in ADC register
+    last_adc_value = adc_value;    
 
+#ifdef DUMP_TO_UART
+    // Convert ADC value to string
+    char buffer[10];
+    snprintf(buffer, sizeof(buffer), "%u\r\n", adc_value);
+    
+    // Send the ADC value over UART
+    uart_sendString(buffer);
+#endif
+}
+
+void ADC_Init() {
+    // Set reference voltage to AVCC and left adjust ADC result
+    ADMUX = (1 << REFS0);
+    // Enable ADC and ADC interrupt, set prescaler to 64
+    ADCSRA = (1 << ADEN) | (1 << ADIE) | (1 << ADPS2) | (1 << ADPS1);
+}
+
+void ADC_Start() {
+    // Start conversion by setting ADSC
+    ADCSRA |= (1 << ADSC);
+}
+
+void frequencyOutputEnable()
+{
     // 3. Konfigurace TCCR1A
     // COM1A0: Přepne (toggle) pin OC1A při každé shodě (Match)
     TCCR1A = (1 << COM1A0);
@@ -71,6 +113,26 @@ void main(void) {
     // WGM12: Zapne režim CTC (Clear Timer on Compare Match)    
     // prescaler 64
     TCCR1B = (1 << WGM12) | (1 << CS11) | (1 << CS10);
+}
+
+void frequencyOutputDisable()
+{
+    // 3. Konfigurace TCCR1A
+    // COM1A0: Přepne (toggle) pin OC1A při každé shodě (Match)
+    TCCR1A &= ~(1 << COM1A0);
+
+    // 4. Konfigurace TCCR1B
+    // WGM12: Zapne režim CTC (Clear Timer on Compare Match)    
+    // prescaler 64
+    TCCR1B &= ~((1 << WGM12) | (1 << CS11) | (1 << CS10));
+}
+
+
+void main(void) {
+    // 1. Nastavení pinu PB1 (OC1A) jako výstupní
+    DDRB |= (1 << PB1);
+
+    frequencyOutputEnable();    
 
     // Nastavení INT0 na náběžnou hranu
     MCUCR |= (1 << ISC01) | (1 << ISC00);
@@ -82,16 +144,20 @@ void main(void) {
     TIMSK |= (1 << TOIE0); // Povolení přerušení při přetečení Timer0
 	
     timer_overflows = 0;
+#ifdef DUMP_TO_UART    
     uart_init();
+#endif    
+    ADC_Init();
 	sei();
 	
     while (1) {
         // Zde můžete měnit OCR1A pro změnu střídy
 		_delay_ms(500);
 
+    #ifdef DUMP_TO_UART
         uart_send_char('T');    
         uart_send_char(':');
-        //uart_send_hex((final_time >> 8) & 0xFF); // vyšší byte
+        
         uart_send_hex(lastTimeHi);        // nižší byte
         uart_send_hex(lastTimeLo);        // nižší byte        
         if (overlowMeasure == 1) {
@@ -100,6 +166,9 @@ void main(void) {
         }
         uart_send_char('\r');
         uart_send_char('\n');
+    #endif
+
+        ADC_Start(); // Spustit ADC konverzi
         
     }
 }
